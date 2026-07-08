c658991683f4f6c65c8975bbda7068a37c9b5f01
amy861400-web <amy861400@gmail.com>
2026-07-08 21:16:41 +0800

    Integrate LINE Bot API into Flask application

--- app.py
+++ app.py
@@ -1,10 +1,48 @@
-from flask import Flask
+from flask import Flask, request
+from linebot import LineBotApi, WebhookHandler
+from linebot.models import TextSendMessage
+from linebot.exceptions import InvalidSignatureError
+import os
 
 app = Flask(__name__)
+
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
 
 @app.route("/")
 def home():
     return "LINE Bot OK"
+
+
+@app.route("/callback", methods=["POST"])
+def callback():
+
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        return "Invalid signature", 400
+
+    return "OK"
+
+
+@handler.add
+def handle_message(event):
+    text = event.message.text
+
+    reply = "收到：" + text
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply)
+    )
+
 
 if __name__ == "__main__":
     app.run()
